ca61f293af4e978a737d58d80cabb53e7d5ba759
Davincible <david.brouwer.99@gmail.com>
2022-11-05 18:07:20 +0100

    i3 change lockscreen to google's xsecurelock

--- i3/lock-screen.sh
+++ i3/lock-screen.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-export XSECURELOCK_COMPOSITE_OBSCURER=0
-export XSECURELOCK_BACKGROUND_COLOR="#101010"
-export XSECURELOCK_AUTH_BACKGROUND_COLOR="#101010"
-
 # Run screen locker
 # Dim after 5min, lock 8s after dimmed
 xset s 300 8
-xss-lock -n /usr/lib/xsecurelock/dimmer -l -- xsecurelock
+xss-lock -n /usr/lib/xsecurelock/dimmer -l -- ~/.config/i3/lock.sh
